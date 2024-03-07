fc6afd3b1cea89ef701521ba8aca186daa318520
David21x10 <davidfer21.yt@gmail.com>
2024-03-07 15:34:38 -0600

    Cambios

--- LacteosCaleb/FrmFactura.Designer.cs
+++ LacteosCaleb/FrmFactura.Designer.cs
@@ -174,7 +174,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column5});
-            this.dataGridView1.Location = new System.Drawing.Point(31, 276);
+            this.dataGridView1.Location = new System.Drawing.Point(47, 279);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.Size = new System.Drawing.Size(564, 123);
